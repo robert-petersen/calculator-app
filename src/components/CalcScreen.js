888d6c1d4dcaebfee11256985188b9ee050c9c29
Robert Petersen <robert.petersen808@gmail.com>
2021-08-25 19:46:03 -0500

    Screen done until javascript needed

--- src/components/CalcScreen.js
+++ src/components/CalcScreen.js
@@ -3,7 +3,9 @@ import React from 'react';
 const CalcScreen = () => {
   return (
     <div className="calculator-screen">
-      
+      <h1>first#</h1>
+      <h1>+</h1>
+      <h1>second#</h1>
     </div>
   )
 }
